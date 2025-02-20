a564300ffc5ebe7a1fc5d4b005ea3404ac71149a
lx <lx@lx.com>
2025-02-20 12:12:47 +0800

    init project basic

---
+++ basic/target/debug/basic.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/zheng/OneDrive/codes/rst/basic/target/debug/basic: /mnt/c/Users/zheng/OneDrive/codes/rst/basic/src/main.rs
---
+++ basic/target/debug/deps/basic-f8bda8d782a91914.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/zheng/OneDrive/codes/rst/basic/target/debug/deps/basic-f8bda8d782a91914: src/main.rs
+
+/mnt/c/Users/zheng/OneDrive/codes/rst/basic/target/debug/deps/basic-f8bda8d782a91914.d: src/main.rs
+
+src/main.rs:
